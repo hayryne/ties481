3911d3595f9eb903815f9d40b260c9c7a14f1cdc
Vahv <vahvett@gmail.com>
2020-11-12 13:48:59 +0200

    JavaSim

---
+++ JavaSim/examples/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module JavaSim {
+	exports org.javasim.simset;
+	exports org.javasim.tests;
+	exports org.javasim.tests.streams;
+	exports org.javasim.tests.examples.stats;
+	exports org.javasim.tests.internal;
+	exports org.javasim.tests.examples.interrupt;
+	exports org.javasim.tests.examples.basic;
+	exports org.javasim;
+	exports org.javasim.tests.stats;
+	exports org.javasim.stats;
+	exports org.javasim.examples.interrupt;
+	exports org.javasim.examples.stats;
+	exports org.javasim.streams;
+	exports org.javasim.internal;
+	exports org.javasim.examples.streams;
+	exports org.javasim.tests.examples.streams;
+	exports org.javasim.examples.basic;
+}
